37164f45c94b664df3708ea6a5f953df712a86e7
Jose Emilio <jcastaneda-2023314@kinal.edu.gt>
2026-06-19 12:05:48 -0600

    feat(Autor) Se agregaron sin implementaciones el DAO impl #25

--- src/org/miguelio/model/Autor.java
+++ src/org/miguelio/model/Autor.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.miguelio.model;
 
-/**
- *
- * @author informatica
- */
 public class Autor {
+//atributos de clase  
+    long cui;
+    String nombre;
+    String apellido;
+    String correoElectronico;
     
-}
+     //contructores
+    public Autor (){
+    }
+    public Autor(long cui, String nombre, String apellido, String correoElectronico)
+            this.cui = cui;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.correoElectronico;
+    }
+    //getter and setters
+    
+
