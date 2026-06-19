72695cca3ca67aac8ae23a9cbc0a996a6b484829
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-06-19 12:04:58 -0600

    feat(Cliente) Se agrego interfaz DAO para cliente #6.5

---
+++ src/org/miguelio/dao/impl/ClienteDAOImpl.java
@@ -0,0 +1,38 @@
+package org.miguelio.dao.impl;
+
+
+package org.miguelio.dao.ClienteDAOImpl;
+
+import org.miguelio.model.Cliente;
+import org.miguelio.dao.ClienteDAO;
+
+import java.util.List;
+
+public class ClienteDAOImpl implements ClienteDAO{
+
+    @Override
+    public boolean insertar(Cliente cliente) 
+    return false;
+    }
+
+    @Override
+    public List<Cliente> listar() {
+return null;
+    }
+
+    @Override
+    public Cliente buscar(long cui){
+return null;{
+    }
+
+    @Override
+    public boolean actualizar(Cliente cliente) {
+ return false;
+    }
+
+    @Override
+    public boolean eliminar(long cui) {
+ return false;
+    }
+
+}
