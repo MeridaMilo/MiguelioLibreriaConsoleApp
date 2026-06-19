88b09657d1480f798703d2844ba84460a17643d2
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-06-19 11:15:46 -0600

    feat(cliente) se agregro modulo cliente #4

---
+++ src/org/miguelio/model/Cliente.java
@@ -0,0 +1,71 @@
+package org.miguelio.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class Cliente {
+    /*
+create table clientes(
+	cui bigint primary key,
+    nombre_cliente varchar(100),
+    apellido_cliente varchar(100),
+    correo_electronico varchar(100)
+);
+    */
+ // atributos clase
+  // verificar todo
+    // cambios para guardar programa
+    long cui;
+    protected String nombre;
+    String apellido;
+    String correoElectronico;
+    
+    // construsctures
+    public Cliente() {
+    }
+    
+    public Cliente(long cui, String nombre, String apellido, String correoElectronico){
+     this.cui = cui;
+     this.nombre = nombre;
+     this.apellido = apellido;
+     this.correoElectronico = correoElectronico;
+     
+    }
+
+    public long getCui() {
+        return cui;
+    }
+
+    public void setCui(long cui) {
+        this.cui = cui;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+            // Validacion: Cambiar mayuscula, 
+            String nombreMayusculas = nombre.toUpperCase();
+        this.nombre = nombreMayusculas;
+  
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+}
