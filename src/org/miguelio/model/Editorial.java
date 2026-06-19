4dd08046dc14653def682ec4e5b5150cea4312b4
Gabriel Escobedo <gescobedo-2023401@kinal.edu.gt>
2026-06-19 11:01:49 -0600

    feat(editorial) Se agrego la clase POJO para Editorial #10

---
+++ src/org/miguelio/model/Editorial.java
@@ -0,0 +1,58 @@
+package org.miguelio.model;
+
+import java.util.logging.Logger;
+
+public class Editorial {
+   //nit varchar(20) primary key,
+    //nombre_editorial varchar(100) not null,
+    //telefono_editorial varchar(15),
+   // direccion_editoria varchar(100)
+   long nit; 
+   String nombre;
+   String telefono;
+   String direccion;
+ //Constructores 
+
+    public Editorial() {
+    }
+
+    public Editorial(long nit, String nombre, String telefono, String direccion) {
+        this.nit = nit;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+// Getter and setters 
+
+    public long getNit() {
+        return nit;
+    }
+
+    public void setNit(long nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
