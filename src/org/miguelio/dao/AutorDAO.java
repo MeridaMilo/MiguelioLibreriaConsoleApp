a49525a2582fe271fb7d6c989b083c371c04825a
Jose Emilio <jcastaneda-2023314@kinal.edu.gt>
2026-07-09 11:17:55 -0600

    feat(autor) Se agrego el DaOimpl para la clase autor #25

--- src/org/miguelio/dao/AutorDAO.java
+++ src/org/miguelio/dao/AutorDAO.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package org.miguelio.dao;
 
-/**
- *
- * @author informatica
- */
-public interface AutorDAO {
-    
+package org.miguelio.dao.impl;
+import java.util.ArrayList;
+import org.miguelio.model.Autor;
+import org.miguelio.dao.AutorDAO;
+import java.util.List;
+ // segundo
+ 
+public class AutorDAOimpl implements AutorDAO {
+ 
+    @Override
+    public boolean insertar(Autor autor) {
+        return false;
+    }
+ 
+    @Override
+    public List<Autor> listar(){
+        //crear lista
+        List<Autor> Autor = new ArrayList ();//nuññ
+        //crear nuestras consulta
+       String consulta = "{call sp_listarcategoria()}"; 
+    //mapear el resultado de la consulta a objeto y lo agregamos
+   try (conexion){
+   }catch (Exception e){
+   }
+
+// retornamos una lista 
+       return null;
+
+ 
+    @Override
+    public Autor buscar(int cui) {
+        return null;
+    }
+ 
+    @Override
+    public boolean actualizar(Autor autor) {
+        return false;
+    }
+ 
+    @Override
+    public boolean eliminar(long cui) {
+        return false;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
