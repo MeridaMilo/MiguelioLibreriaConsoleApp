0708a955cb2ebab816ae83125f1d27d2d9b3aa92
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-07-10 11:21:15 -0600

    feat.(Cliente) Se agrego utils Conexion

--- src/org/miguelio/utils/Conexion.java
+++ src/org/miguelio/utils/Conexion.java
@@ -5,25 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
  
 public class Conexion {
+    private static Conexion instancia;
+    // Configuración del string de conexión, y credenciales
     private static final String URL = "jdbc:mysql://localhost:3306/libreriadb_in4cm?serverTimezone=UTC";
     private static final String USER = "IN4CM";
     private static final String PASSWORD = "#NdimAM4";
-    public static Connection conectar(){
-        Connection conexion = null;
+ 
+    //Constructor privado para evitar que hagan "new Conexion()" fuera de esta clase
+    private Conexion() {
         try {
-            // driver de JDBC
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //crear la conexion 
-            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
-            System.err.print("ERROR: no hay driver");
-        } catch (SQLException ex){
-            System.err.print("ERROR: no se pudo conectar a la DB"+ ex.getMessage());
+            System.err.println("Error Driver: " + e.getMessage());
         }
-        return conexion;
+    }
+ 
+    //Método público estático para obtener la única instancia del Gestor
+    public static synchronized Conexion getInstancia() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+ 
+    //Método para entregar una conexión fresca cada vez que se pida
+    public Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
-    public static Object getInstancia() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 }
